88275b3473fc62761c19a69dadcebc55f921499a
israrahmedjan <123giIsrar456>
2025-02-07 21:24:52 +0500

    image change

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import SearchBox from "./_components/produccts/SearchBox";
 export default function Home() {
   return (
     <>
-    <div className="py-2 px-8 mt-[120px] min-h-[450px] h-full">
+    <div className="py-2 px-8 mt-[120px] min-h-[600px] overflow-scroll">
        {/* <SearchBox /> */}
     <Products />
     </div>
