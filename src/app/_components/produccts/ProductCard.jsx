7cc72e3088629000cb266b2a05e8f15815ddf68f
israrahmedjan <123giIsrar456>
2025-02-09 19:39:16 +0500

    chksdfj

--- src/app/_components/produccts/ProductCard.jsx
+++ src/app/_components/produccts/ProductCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { motion } from "framer-motion";
-import { ShoppingCart, Heart, Scale ,RefreshCw,Eye } from "lucide-react";
+import { ShoppingCart, Heart, Scale ,RefreshCw,Eye,Star  } from "lucide-react";
 
 function ProductCard({product}) {
   return (
@@ -9,13 +9,13 @@ function ProductCard({product}) {
 
 
 
-    <div className='group relative  p-2 thin-border  '>
+    <div className='group relative rounded-lg mt-2 mx-1  p-2 thin-border text-[12px] lg:text-[14px] shadow-lg'>
     <img
           src="images/product.jpg"
           alt={product.productName}
           className="w-full h-[250px] object-cover rounded-md transition-transform duration-300 group-hover:scale-105"
         />
-        <div className='absolute top-3  right-3 flex flex-col gap-2'>
+        <div className='absolute top-3  right-3 flex flex-col gap-2 '>
        <button className="p-2 bg-white shadow-md rounded-full hover:bg-gray-100 transition">
             <Heart className="h-5 w-5 text-secondary" />
           </button>
@@ -29,9 +29,18 @@ function ProductCard({product}) {
           </div>
           {/* Product Details */}
       <div className="mt-4 text-center p-2 ">
-        <h3 className="font-semibold text-primary">{product.productName}</h3>
-        <div className='flex justify-between items-center'>
-        <p className="flex px-3 py-1 thin-border rounded-lg text-secondary"><span>Add To Cart</span><ShoppingCart /></p>
+        <div className='flex justify-between items-center lg:text-[14px] mb-2 italic'>
+          <span className=''>{product.categoryName}</span>
+      <span className=" flex flex-row items-center  text-primary">
+      {Array.from({ length: 6 }).map((_, i) => (
+  <div className='text-secondary' key={i}><Star size={15} /></div>
+))}
+        </span>
+      </div>
+     <div className='flex pb-3 thin-border-bottom '> <h3 className="font-semibold text-left text-primary">{product.productName}</h3></div>
+       
+        <div className='flex justify-between items-center '>
+        <p className="flex px-3 py-1 thin-border items-center gap-3 mt-3 rounded-lg text-secondary hover:bg-gray-100 cursor-pointer"><ShoppingCart /><span className='text-primary'>Add To Cart</span></p>
         <p className="text-gray-600">${product.price}</p>
        
         </div>
