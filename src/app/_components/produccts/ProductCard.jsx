d6d8401af040c1c287f74b09aea9fcfa4ba6c35c
israrahmedjan <123giIsrar456>
2025-02-09 17:38:17 +0500

    Card added

--- src/app/_components/produccts/ProductCard.jsx
+++ src/app/_components/produccts/ProductCard.jsx
@@ -5,16 +5,17 @@ import { ShoppingCart, Heart, Scale ,RefreshCw} from "lucide-react";
 function ProductCard({product}) {
   return (
     <>
-<div className="group relative bg-white rounded-xl shadow-md lg:text-[14px] overflow-hidden border border-gray-200 hover:shadow-lg transition duration-300 w-72">
+
+<div className="group relative bg-white rounded-xl shadow-md text-sm overflow-hidden border border-gray-200 hover:shadow-lg transition duration-300 w-full max-w-[280px]">
       {/* Product Image */}
-      <div className="relative overflow-hidden">
+      <div className="relative overflow-hidde">
         {/* Default Image */}
         <img
           src="images/product.jpg"
-          alt="Product"
-          className="w-full h-64 object-cover rounded-md transition-transform duration-300 group-hover:scale-105"
+          alt={product.productName}
+          className="w-full h-[250px] object-cover rounded-md transition-transform duration-300 group-hover:scale-105"
         />
-        
+
         {/* Overlay Icons (Wishlist & Compare) */}
         <div className="absolute top-3 right-3 flex flex-col space-y-2 opacity-0 group-hover:opacity-100 transition duration-300">
           <button className="p-2 bg-white shadow-md rounded-full hover:bg-gray-100 transition">
@@ -33,7 +34,7 @@ function ProductCard({product}) {
       </div>
 
       {/* Product Details */}
-      <div className="mt-4 text-center">
+      <div className="mt-4 text-center p-2">
         <h3 className="font-semibold text-primary">{product.productName}</h3>
         <p className="text-gray-600">${product.price}</p>
       </div>
