158a8ef6b5b2dc3fe583c51ce20cc88567441f8c
israrahmedjan <123giIsrar456>
2025-02-18 22:40:21 +0500

    some new changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    images: {
+      domains: ['res.cloudinary.com','freepik.com'],
+    },
+  };
 export default nextConfig;
