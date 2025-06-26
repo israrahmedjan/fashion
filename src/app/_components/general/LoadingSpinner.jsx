8979f72022e70fa2bd052a609a024dec7a423f34
israrahmedjan <123giIsrar456>
2025-06-26 12:23:31 +0500

    new change

--- src/app/_components/general/LoadingSpinner.jsx
+++ src/app/_components/general/LoadingSpinner.jsx
@@ -1,8 +1,8 @@
 // components/LoadingSpinner.jsx
 export default function LoadingSpinner() {
   return (
-    <div className="flex items-center justify-center h-20">
-      <div className="w-8 h-8 border-4 border-[#323232] border-t-transparent rounded-full animate-spin" />
+    <div className="flex flex-col gap-3 items-center justify-center h-20">
+      <div className="w-8 h-8  border-4 border-[#323232] border-t-transparent rounded-full animate-spin" />
       <div>Loading...</div>
     </div>
   );
